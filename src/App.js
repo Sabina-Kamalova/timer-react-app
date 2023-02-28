0671e6388c227034e1895d446d606b52bdb45565
Sabina Kamalova <skamalova09@gmail.com>
2023-02-28 20:44:43 +0100

    Add timer && styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import Timer from './components/Timer/Timer';
+import { useState } from 'react';
+//import { useEffect } from '.react';
+import Button from './components/Button/Button';
+import styles from './App.module.scss';
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const start = () => {
+   setTimer(setInterval(() => {
+     setTime(prevValue => prevValue + 1);
+   }, 1))
+  };
+
+  const stop = () => {
+    clearInterval(timer);
+  };
+
+  const reset = () => {
+    clearInterval(timer);
+    setTime(0);
+  };
+
   return (
-    <div>
-      <Timer />
+    <div className={styles.container}>
+      <Timer time={ time } />
+      <Button onClick={ start }>Start</Button>
+      <Button onClick={ stop }>Stop</Button>
+      <Button onClick={ reset }>Reset</Button>
     </div>
   );
 }
--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,11 +1,25 @@
-//import styles from './Timer.module.scss';
+import styles from './Timer.module.scss';
 
-const Timer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Timer = ({ time }) => {
+  let miliseconds = Math.floor(time % 1000);
+   const formatTime = time => {
+      let seconds = Math.floor((time / 1000) % 60);
+      let minutes = Math.floor((time / 1000 / 60) % 60);
+      let hours = Math.floor((time / 1000 / 60 / 60) % 24);
+
+      return [
+        hours.toString().padStart(2, "0"),
+        minutes.toString().padStart(2, "0"),
+        seconds.toString().padStart(2, "0"),
+      ].join(":");
+    };
+  const formattedTime = formatTime(time);
+  console.log(formattedTime);
+
+  return (
+    <div className={styles.timer}>
+      {formattedTime}.{miliseconds.toString().padStart(2, "0")}
+    </div>
+  )
 }
-
 export default Timer;
